460e4479738a77e6b3e62f0d6dad1aab531120fb
Emisael Kisler <emisael@croney.io>
2024-02-14 17:55:04 -0400

    Programa Nro 15

---
+++ prog15.c
@@ -0,0 +1,16 @@
+/* prog15.c -- Ancho de campo */
+
+#include <stdio.h>
+
+#define PAGINAS 931
+
+int main( void )
+{
+    printf("*%d*\n", PAGINAS);
+    printf("*%2d*\n", PAGINAS);
+    printf("*%10d*\n", PAGINAS);
+    printf( "*123456789012345*\n" );
+    printf("*%-10d*\n", PAGINAS);
+
+    return 0;
+}
